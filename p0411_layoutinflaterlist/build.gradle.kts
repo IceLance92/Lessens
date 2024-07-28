b10508f848d4bb648d5f7d825727affd022a99c7
aleksandr <mograin_92@mail.ru>
2024-07-28 07:52:50 -0700

    все исправил

--- p0411_layoutinflaterlist/build.gradle.kts
+++ p0411_layoutinflaterlist/build.gradle.kts
@@ -3,49 +3,47 @@ plugins {
 }
 
 android {
-<<<<<<< HEAD
     namespace = "com.practicum.p0411_layoutinflaterlist"
     compileSdk = 34
 
     defaultConfig {
         applicationId = "com.practicum.p0411_layoutinflaterlist"
-=======
-    namespace = "ru.startandroid.develop.p0411_layoutinflaterlist"
-    compileSdk = 34
+        namespace = "ru.startandroid.develop.p0411_layoutinflaterlist"
+        compileSdk = 34
 
-    defaultConfig {
-        applicationId = "ru.startandroid.develop.p0411_layoutinflaterlist"
->>>>>>> 4bb4a6c (проверка совместимости с тем)
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        defaultConfig {
+            applicationId = "ru.startandroid.develop.p0411_layoutinflaterlist"
+            minSdk = 24
+            targetSdk = 34
+            versionCode = 1
+            versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+
+    dependencies {
+
+        implementation(libs.appcompat)
+        implementation(libs.material)
+        implementation(libs.activity)
+        implementation(libs.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.ext.junit)
+        androidTestImplementation(libs.espresso.core)
     }
-}
-
-dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
 }
