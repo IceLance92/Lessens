f99b2d049a1591ca0dc56f44a31af719d9a2802a
aleksandr <mograin_92@mail.ru>
2024-08-10 16:39:35 -0700

    Испрпвлена ошибка в задании 581

--- p0581_timepickerdialog/src/main/java/ru/startandroid/develop/p0581_timepickerdialog/MainActivity.java
+++ p0581_timepickerdialog/src/main/java/ru/startandroid/develop/p0581_timepickerdialog/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,33 +16,30 @@ public class MainActivity extends AppCompatActivity {
     int myMinute = 35;
     TextView tvTime;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        tvTime = findViewById(R.id.tvTime);
+        tvTime = (TextView) findViewById(R.id.tvTime);
     }
-    public void onlick(View view) {
+
+    public void onclick(View view) {
         showDialog(DIALOG_TIME);
     }
+
     protected Dialog onCreateDialog(int id) {
         if (id == DIALOG_TIME) {
-            TimePickerDialog tpd = new TimePickerDialog(this,
-                    myCallBack,
-                    myHour,
-                    myMinute,
-                    true);
+            TimePickerDialog tpd = new TimePickerDialog(this, myCallBack, myHour, myMinute, true);
             return tpd;
         }
         return super.onCreateDialog(id);
     }
+
     TimePickerDialog.OnTimeSetListener myCallBack = new TimePickerDialog.OnTimeSetListener() {
-        @Override
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             myHour = hourOfDay;
             myMinute = minute;
-            tvTime.setText("Time is " + myHour + " hors " + myMinute + " minutes");
+            tvTime.setText("Time is " + myHour + " hours " + myMinute + " minutes");
         }
     };
 }
