040ec62f1004ea2aeb570b1e836f4fc548a46ebc
ФAleksandr <mograin_92@mail.ru>
2024-08-12 01:03:32 +0300

    обхедиенение веток

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -56,4 +56,8 @@ include(":p0581_timepickerdialog")
 include(":p0591_datepickerdialog")
 include(":p0601_alertdialogsimple")
 include(":p0611_alertdialogprepare")
+<<<<<<< HEAD
 include(":p0621_alertdialogitems")
+=======
+include(":p0631_alertdialogitemssingle")
+>>>>>>> main
