5658c487097a6dbe7797f9b826825d3ec39e4b23
ФAleksandr <mograin_92@mail.ru>
2024-08-13 03:01:44 +0300

    задание успешно пройдено 661

---
+++ p0661_alertdialogoperations/src/main/java/com/practicum/p0661_alertdialogoperations/MainActivity.java
@@ -0,0 +1,93 @@
+package com.practicum.p0661_alertdialogoperations;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+    final int DIALOG = 1;
+
+    Dialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG) {
+            Log.d(LOG_TAG, "Create");
+            AlertDialog.Builder adb = new AlertDialog.Builder(this);
+            adb.setTitle("Title");
+            adb.setMessage("Message");
+            adb.setPositiveButton("OK", null);
+            dialog = adb.create();
+
+            // обработчик отображения
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    Log.d(LOG_TAG, "Show");
+                }
+            });
+
+            // обработчик отмены
+            dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    Log.d(LOG_TAG, "Cancel");
+                }
+            });
+
+            // обработчик закрытия
+            dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    Log.d(LOG_TAG, "Dismiss");
+                }
+            });
+            return dialog;
+        }
+        return super.onCreateDialog(id);
+    }
+    void method1() {
+        removeDialog(DIALOG);
+    }
+    void method2() {
+        showDialog(DIALOG);
+    }
+    public void onclick(View view) {
+        showDialog(DIALOG);
+
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                method1();
+            }
+        }, 2000);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                method2();
+            }
+        }, 4000);
+    }
+}
