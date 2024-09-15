1494572d232849f663e2f04531b1c5b359eb54e3
ФAleksandr <mograin_92@mail.ru>
2024-09-15 12:47:23 +0300

    изменено задание 113 с долгим нажатием на список

--- p1131_actionmode/src/main/java/com/example/p1131_actionmode/MainActivity.java
+++ p1131_actionmode/src/main/java/com/example/p1131_actionmode/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.p1131_actionmode;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +18,51 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     
     ActionMode actionMode;
+    ListView lvActionMode;
     final String LOG_TAG = "myLogs";
 
+    String[] data = {"one", "two", "three", "four", "five"};
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_activated_1, data);
+        lvActionMode = (ListView) findViewById(R.id.lvActionMode);
+        lvActionMode.setAdapter(adapter);
+        lvActionMode.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        lvActionMode.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+                Log.d(LOG_TAG, "position = " + i + ", checked = " + b);
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                actionMode.getMenuInflater().inflate(R.menu.context, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                actionMode.finish();
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
     }
 
     @Override
@@ -28,34 +70,4 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.context, menu);
         return true;
     }
-    public void onClick(View view) {
-        if (actionMode == null) actionMode = startActionMode(callback);
-        else actionMode.finish();
-    }
-    
-    private final ActionMode.Callback callback = new ActionMode.Callback() {
-        @Override
-        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-            actionMode.getMenuInflater().inflate(R.menu.context, menu);
-            return true;
-        }
-
-        @Override
-        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-            return false;
-        }
-
-        @Override
-        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-            Log.d(LOG_TAG, "item " + menuItem.getTitle());
-            return false;
-        }
-
-        @Override
-        public void onDestroyActionMode(ActionMode actionMode) {
-            Log.d(LOG_TAG, "destroy");
-            actionMode = null;
-
-        }
-    };
 }
